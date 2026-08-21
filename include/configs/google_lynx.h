c0d04cbc87e1579a5a83e8070ab568acf94144a6
Theo Paris <theo@theoparis.com>
2026-08-20 19:04:38 -0700

    Add google gs201 board support

---
+++ include/configs/google_lynx.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Google Pixel 7a (Lynx - Tensor GS201) Board Configuration
+ *
+ * Copyright (C) 2024-2026 Google LLC
+ */
+
+#ifndef __CONFIG_GOOGLE_LYNX_H
+#define __CONFIG_GOOGLE_LYNX_H
+
+#define CFG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, 115200 }
+
+#endif /* __CONFIG_GOOGLE_LYNX_H */
